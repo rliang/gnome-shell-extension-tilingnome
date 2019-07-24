43ac78f05872ae9866cfd12ca6ae1a51e7063f9c
rliang <ricardoliang@gmail.com>
2019-07-24 14:31:54 -0300

    Improve code quality, performance

--- extension.js
+++ extension.js
@@ -38,91 +38,90 @@ function tileInit(win) {
 }
 
 function tileInitAuto(win) {
-  const types = settings.get_strv("auto-tile-window-types");
-  if (types.some(t => win.window_type === Meta.WindowType[t])) tileInit(win);
+  for (const type of settings.get_strv("auto-tile-window-types"))
+    if (win.window_type === Meta.WindowType[type]) return tileInit(win);
 }
 
 function tileDestroy(win) {
   delete win._tilingnome;
 }
 
-function tileData(win) {
+function tileGetData(win) {
   return win._tilingnome;
 }
 
-function tileCompare(w1, w2) {
-  const i1 = tileData(w1);
-  const i2 = tileData(w2);
-  return i1.idx > i2.idx ? 1 : i1.idx < i2.idx ? -1 : 0;
-}
-
-function getWorkspaceTiles() {
-  return global.workspace_manager
-    .get_active_workspace()
-    .list_windows()
-    .filter(tileData)
-    .filter(win => !win.fullscreen && !win.get_maximized() && !win.minimized)
-    .sort(tileCompare);
-}
-
-function refreshTile(win, idx) {
-  const tile = tileData(win);
-  if (tile.idx !== idx) {
-    tile.idx = idx;
+function tileSetIndex(win, idx) {
+  const data = tileGetData(win);
+  if (data.idx !== idx) {
+    data.idx = idx;
     const ming = settings.get_value("minimum-gaps").deep_unpack();
     const maxg = settings.get_value("maximum-gaps").deep_unpack();
-    tile.gaps = {
+    data.gaps = {
       x: ming[0] + Math.random() * (maxg[0] - ming[0]),
       y: ming[1] + Math.random() * (maxg[1] - ming[1]),
       width: ming[2] + Math.random() * (maxg[2] - ming[2]),
       height: ming[3] + Math.random() * (maxg[3] - ming[3])
     };
   }
-  return tile;
+  return data;
 }
 
-function refreshMonitor(mon) {
-  const wksp = global.workspace_manager.get_active_workspace();
-  const wins = getWorkspaceTiles().filter(win => win.get_monitor() === mon);
-  const [x, y, width, height] = settings.get_value("margins").deep_unpack();
-  const area = rectAddGaps(wksp.get_work_area_for_monitor(mon), { x, y, width, height });
+function getTiles() {
+  return global.workspace_manager
+    .get_active_workspace()
+    .list_windows()
+    .filter(tileGetData)
+    .filter(win => !win.fullscreen && !win.get_maximized() && !win.minimized)
+    .sort((w1, w2) => {
+      const t1 = tileGetData(w1);
+      const t2 = tileGetData(w2);
+      return t1.idx > t2.idx ? 1 : t1.idx < t2.idx ? -1 : 0;
+    });
+}
+
+function refresh() {
   const layouts = settings.get_strv("layouts");
   if (!layouts.length) return;
   const layout = Me.imports.layouts[layouts[0]];
   if (!layout) return;
-  layout(settings, wins, area).forEach((rect, idx) => {
-    const win = wins[idx];
-    const oldrect = win.get_frame_rect();
-    const tile = refreshTile(win, idx);
-    rect = rectAddGaps(rect, tile.gaps);
-    win.move_resize_frame(false, rect.x, rect.y, rect.width, rect.height);
-    Meta.later_add(Meta.LaterType.IDLE, () =>
-      Tweener.addTween(win.get_compositor_private(), {
-        transition: settings.get_string("animation-transition"),
-        time: settings.get_double("animation-duration"),
-        scale_x: 1,
-        scale_y: 1,
-        translation_x: 0,
-        translation_y: 0,
-        onStart: (actor, r1, r2) => {
-          if ((actor.translation_x = r1.x - r2.x) > 0)
-            actor.translation_x -= r2.width - r1.width;
-          if ((actor.translation_y = r1.y - r2.y) > 0)
-            actor.translation_y -= r2.height - r1.height;
-          actor.set_pivot_point(r2.x >= r1.x ? 0 : 1, r2.y >= r1.y ? 0 : 1);
-          actor.set_scale(r1.width / r2.width, r1.height / r2.height);
-        },
-        onStartParams: [win.get_compositor_private(), oldrect, rect]
-      })
-    );
-  });
-}
-
-function refresh() {
-  Main.layoutManager.monitors.forEach((_, m) => refreshMonitor(m));
+  const [x, y, width, height] = settings.get_value("margins").deep_unpack();
+  const margins = { x, y, width, height };
+  const workspace = global.workspace_manager.get_active_workspace();
+  for (const monitor of Main.layoutManager.monitors) {
+    const area = rectAddGaps(workspace.get_work_area_for_monitor(monitor.index), margins);
+    const tiles = getTiles().filter(tile => tile.get_monitor() === monitor.index);
+    const rects = layout(settings, tiles, area);
+    for (let i = 0; i < tiles.length; i++) {
+      const tile = tiles[i];
+      const data = tileSetIndex(tile, i);
+      const rect = rectAddGaps(rects[i], data.gaps);
+      const oldrect = tile.get_frame_rect();
+      tile.move_resize_frame(false, rect.x, rect.y, rect.width, rect.height);
+      Meta.later_add(Meta.LaterType.IDLE, () =>
+        Tweener.addTween(tile.get_compositor_private(), {
+          transition: settings.get_string("animation-transition"),
+          time: settings.get_double("animation-duration"),
+          scale_x: 1,
+          scale_y: 1,
+          translation_x: 0,
+          translation_y: 0,
+          onStart: (actor, r1, r2) => {
+            if ((actor.translation_x = r1.x - r2.x) > 0)
+              actor.translation_x -= r2.width - r1.width;
+            if ((actor.translation_y = r1.y - r2.y) > 0)
+              actor.translation_y -= r2.height - r1.height;
+            actor.set_pivot_point(r2.x >= r1.x ? 0 : 1, r2.y >= r1.y ? 0 : 1);
+            actor.set_scale(r1.width / r2.width, r1.height / r2.height);
+          },
+          onStartParams: [tile.get_compositor_private(), oldrect, rect]
+        })
+      );
+    }
+  }
 }
 
 const _handles = [];
+const _bindings = [];
 
 function addSignal(obj, name, handler) {
   _handles.push([obj, obj.connect(name, handler)]);
@@ -136,6 +135,7 @@ function addKeybinding(name, handler) {
     Shell.ActionMode.NORMAL,
     handler
   );
+  _bindings.push(name);
 }
 
 function enable() {
@@ -150,7 +150,7 @@ function enable() {
   addSignal(global.window_manager, "switch-workspace", refresh);
   addSignal(global.display, "grab-op-end", (_0, _1, w1, op) => {
     if (op !== Meta.GrabOp.MOVING) return;
-    const i1 = tileData(w1);
+    const i1 = tileGetData(w1);
     if (!i1) return;
     const [px, py, pmask] = global.get_pointer();
     const p = new Meta.Rectangle({ x: px, y: py, width: 1, height: 1 });
@@ -160,25 +160,25 @@ function enable() {
     if (!m) return;
     p.x -= m.x;
     p.y -= m.y;
-    const w2 = getWorkspaceTiles().find(
+    const w2 = getTiles().find(
       w =>
         w !== w1 &&
         w.get_monitor() === m.index &&
         w.get_frame_rect().intersect(p)[0]
     );
     if (!w2) return;
-    const i2 = tileData(w2);
+    const i2 = tileGetData(w2);
     if (!i1 || !i2) return;
     const tmp = i1.idx;
-    refreshTile(w1, i2.idx);
-    refreshTile(w2, tmp);
+    tileSetIndex(w1, i2.idx);
+    tileSetIndex(w2, tmp);
     refresh();
     return;
   });
   addKeybinding("toggle-tile", () => {
     const win = global.display.get_focus_window();
     if (!win) return;
-    if (tileData(win)) tileDestroy(win);
+    if (tileGetData(win)) tileDestroy(win);
     else tileInit(win);
     refresh();
   });
@@ -194,52 +194,49 @@ function enable() {
   });
   addKeybinding("focus-next-tile", () => {
     const w1 = global.display.get_focus_window();
-    const w2 = arrayNeighbor(getWorkspaceTiles(), w1, 1);
+    const w2 = arrayNeighbor(getTiles(), w1, 1);
     if (w2) w2.focus(global.get_current_time());
   });
   addKeybinding("focus-previous-tile", () => {
     const w1 = global.display.get_focus_window();
-    const w2 = arrayNeighbor(getWorkspaceTiles(), w1, -1);
+    const w2 = arrayNeighbor(getTiles(), w1, -1);
     if (w2) w2.focus(global.get_current_time());
   });
   addKeybinding("focus-first-tile", () => {
-    const w2 = getWorkspaceTiles()[0];
+    const w2 = getTiles()[0];
     if (w2) w2.focus(global.get_current_time());
   });
   addKeybinding("swap-next-tile", () => {
     const w1 = global.display.get_focus_window();
-    const w2 = arrayNeighbor(getWorkspaceTiles(), w1, 1);
+    const w2 = arrayNeighbor(getTiles(), w1, 1);
     if (!w1 || !w2) return;
-    const i1 = tileData(w1),
-      i2 = tileData(w2);
+    const i1 = tileGetData(w1), i2 = tileGetData(w2);
     if (!i1 || !i2) return;
     const tmp = i1.idx;
-    refreshTile(w1, i2.idx);
-    refreshTile(w2, tmp);
+    tileSetIndex(w1, i2.idx);
+    tileSetIndex(w2, tmp);
     refresh();
   });
   addKeybinding("swap-previous-tile", () => {
     const w1 = global.display.get_focus_window();
-    const w2 = arrayNeighbor(getWorkspaceTiles(), w1, -1);
+    const w2 = arrayNeighbor(getTiles(), w1, -1);
     if (!w1 || !w2) return;
-    const i1 = tileData(w1),
-      i2 = tileData(w2);
+    const i1 = tileGetData(w1), i2 = tileGetData(w2);
     if (!i1 || !i2) return;
     const tmp = i1.idx;
-    refreshTile(w1, i2.idx);
-    refreshTile(w2, tmp);
+    tileSetIndex(w1, i2.idx);
+    tileSetIndex(w2, tmp);
     refresh();
   });
   addKeybinding("swap-first-tile", () => {
     const w1 = global.display.get_focus_window();
-    const w2 = getWorkspaceTiles()[0];
+    const w2 = getTiles()[0];
     if (!w1 || !w2) return;
-    const i1 = tileData(w1),
-      i2 = tileData(w2);
+    const i1 = tileGetData(w1), i2 = tileGetData(w2);
     if (!i1 || !i2) return;
     const tmp = i1.idx;
-    refreshTile(w1, i2.idx);
-    refreshTile(w2, tmp);
+    tileSetIndex(w1, i2.idx);
+    tileSetIndex(w2, tmp);
     refresh();
   });
   addKeybinding("increase-split", () => {
@@ -258,24 +255,16 @@ function enable() {
   addKeybinding("decrease-master-count", () =>
     settings.set_uint("master-count", settings.get_uint("master-count") - 1)
   );
-  global.get_window_actors().forEach(w => tileInitAuto(w.meta_window));
+  for (const actor of global.get_window_actors())
+    tileInitAuto(actor.meta_window);
   refresh();
 }
 
 function disable() {
-  for (const [obj, handle] of _handles) obj.disconnect(handle);
-  Main.wm.removeKeybinding("toggle-tile");
-  Main.wm.removeKeybinding("switch-next-layout");
-  Main.wm.removeKeybinding("switch-previous-layout");
-  Main.wm.removeKeybinding("focus-next-tile");
-  Main.wm.removeKeybinding("focus-previous-tile");
-  Main.wm.removeKeybinding("focus-first-tile");
-  Main.wm.removeKeybinding("swap-next-tile");
-  Main.wm.removeKeybinding("swap-previous-tile");
-  Main.wm.removeKeybinding("swap-first-tile");
-  Main.wm.removeKeybinding("increase-split");
-  Main.wm.removeKeybinding("decrease-split");
-  Main.wm.removeKeybinding("increase-master-count");
-  Main.wm.removeKeybinding("decrease-master-count");
-  global.get_window_actors().forEach(w => tileDestroy(w.meta_window));
+  for (const [obj, id] of _handles.splice(0, _handles.length))
+    obj.disconnect(id);
+  for (const name of _bindings.splice(0, _bindings.length))
+    Main.wm.removeKeybinding(name);
+  for (const actor of global.get_window_actors())
+    tileDestroy(actor.meta_window);
 }
